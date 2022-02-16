5ceb34b5215aa48a5e64353b19788e24d0307051
kdhone24011996 <kdhone24011996@gmail.com>
2022-02-16 18:37:05 +0530

    changes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.use('/test', (req, res,next)=>{
-    res.send('success change')
+    res.send(`server is running on port ${process.env.PORT}`)
 })
 
+const port = process.env.PORT || 8001
 
-
-app.listen(process.env.PORT || 8001)
+app.listen(port)
 
